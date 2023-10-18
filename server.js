5455dfd021629ae4d7d22d7c98e7c33fa7f645dc
Surasak <92101184+Surasak0510@users.noreply.github.com>
2023-10-18 20:24:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ DB.connect((err) => {
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://2tsneaker.vercel.app");
-    res.header("Access-Control-Allow-Origin", "localhost:3000");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
