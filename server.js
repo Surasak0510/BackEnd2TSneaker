db841740008190d0ea1d44f16ca8ec0a71e60b32
Surasak <92101184+Surasak0510@users.noreply.github.com>
2023-10-18 19:03:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ DB.connect((err) => {
 })
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
